4ded808b40753a3d451a9a8d82a8b9b7724d9276
Bernard Toplak <bernard@orion-web.hr>
2016-01-10 09:39:53 +0100

    Version 1.0	[2016-01-10] * added: target extension check * added: option to define plain-text file extensions, only which will be cleaned from garbage * added: PHP version check * added: counter for files cleaned from garbage * fixed: issue with remaining newline character in last 16 bytes * improved: encrypter garbage cleanup function * improved: extension check * removed: erroneous variables

--- PLED.php
+++ PLED.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * PHP "Linux.Encoder" Decrypter - PLED
- * @version 1.0-RC2
+ * @version 1.0-RC3
  *
  * @author Bernard Toplak <bernard@php-antivirus.com>
  * @link http://www.php-antivirus.com
@@ -31,8 +31,10 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 ini_set('output_buffering', '0'); // disable output buffering
 ini_set('implicit_flush', '1'); // disable output buffering
 
+
 $ignoreDirFiles = array('.','..','.DS_Store','.svn','.git','README.md'); // dirs/files to ignore
-$encryptedExtension = '.encrypted';
+$encryptedExtension = 'encrypted'; // extension of the encrypted files
+$plaintextExtensions = array('php','txt','js','css','htm','html','ini'); // for which files to fix last garbage bytes
 $dir4scan = array('.'); // a directory to scan; default: current dir
 $targetDir = 'PLED_decrypted';
 /* * * * * * * * * * * * * *  END SETTINGS  * * * * * * * * * * * * * * */
@@ -44,6 +46,10 @@ if (!extension_loaded('mbstring'))
     die ('PHP Extension "mbstring" not loaded');
 if (!extension_loaded('mcrypt'))
     die ('PHP Extension "mcrypt" not loaded');
+
+if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+die( 'You are using PHP Version: '. PHP_VERSION .'. You have to deploy at least PHP 5.3.0 to be able to use this script!');
+}
 
 /* * * * * * * * * * * * * *  FUNCTIONS  * * * * * * * * * * * * * * */
 
@@ -61,8 +67,11 @@ if ( !function_exists( 'hex2bin' ) ) { # exists only in PHP > 5.4
 function decrypt_file($filePath) {
     global $encryptedExtension;
     global $targetDir;
+    global $plaintextExtensions;
+    global $filesCleanedCount;
 
-    $decryptedFilePath = $targetDir.DIRECTORY_SEPARATOR.mb_substr($filePath, 2, -strlen($encryptedExtension));
+    $decryptedFilePath = $targetDir.'/'.mb_substr($filePath, 2, -strlen($encryptedExtension)-1);
+    
     if (file_exists($decryptedFilePath)) {
         smart_echo("File $decryptedFilePath already exists, skipping to the next one ...\n");
         return false;
@@ -76,13 +85,13 @@ function decrypt_file($filePath) {
         return false;
     }
     
-    $RSAenc = fread($encFileHandle, 256);
+    // $RSAenc = fread($encFileHandle, 256); # unused, to be removed
+    fseek($encFileHandle, 256, SEEK_CUR);
     $IV = fread($encFileHandle, 16);
     $AESkey = $IV."\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
     $padding = ord($IV[15]) & 0x0F;
-    
-    $prev = $IV;
     $written = 0;
+    
     while (true) {
             $ciphertext = fread($encFileHandle, 32);
             if(!$ciphertext)
@@ -90,14 +99,19 @@ function decrypt_file($filePath) {
                 $last_bytes = $decrypted;
                 break;
             }
-            $decrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_128,$AESkey,$ciphertext,MCRYPT_MODE_CBC,$prev);
+            $decrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_128,$AESkey,$ciphertext,MCRYPT_MODE_CBC,$IV);
             fwrite($decFileHandle, $decrypted);
             $written += 32;
-            $prev = mb_substr($ciphertext, 0, 16);
+            $IV = mb_substr($ciphertext, 0, 16);
     }
     
-    if ($padding != 0) 
-        washmachine($decryptedFilePath, $decFileHandle, $written, $padding, $last_bytes);
+    $targetExtension = end(explode('.',$decryptedFilePath));
+    if ($padding != 0 && in_array($targetExtension, $plaintextExtensions)) {
+         smart_echo("File $decryptedFilePath will be cleaned from garbage bytes.\n");
+         # copy($decryptedFilePath, $decryptedFilePath.'.raw');
+         $filesCleanedCount += 1;
+         washmachine($decFileHandle, $written, $padding, $last_bytes);
+    }
 
     fclose ($decFileHandle);
     fclose ($encFileHandle);
@@ -106,18 +120,21 @@ function decrypt_file($filePath) {
 }
 
 
-function washmachine($decryptedFilePath, $decFileHandle, $written, $padding, $last_bytes) {
-    
+function washmachine($decFileHandle, $written, $padding, $last_bytes) {
     $correction = 0;
     $truncated_lines = substr($last_bytes, 0, -(16 - $padding) );
-    
+    # check if last char is a newline
+    if (substr($truncated_lines, -1, 1) == "\x0A" ) {
+        $truncated_lines = substr($truncated_lines, 0, -1);
+        $correction += 1;
+    }
+    # check if last 16 bytes are printable
     if (strlen($truncated_lines) >= 16) {
         $last_16 = substr($truncated_lines,-16,16);
         $line_printable = ctype_print($last_16);
         if (!$line_printable) 
-            $correction = 16;
+            $correction += 16;
     }
-    
     ftruncate($decFileHandle, $written -(16 + $correction - $padding));    
 }
 
@@ -131,7 +148,7 @@ function smart_echo($string){
 }
 
 
-$fileCount = $fileEncCount = $folderCount = $start = $end = $sumSize = $runTime = 0;
+$fileCount = $fileEncCount = $folderCount = $sumSize = 0;
 $start = microtime();
 while ($dir4scan) {
     $thisDir = array_pop($dir4scan);
@@ -139,17 +156,18 @@ while ($dir4scan) {
         foreach ($dirContent As $content) {
             if (!in_array($content, $ignoreDirFiles)) {
                 $thisFile = "$thisDir/$content";
+                $fileInfo = pathinfo($thisFile);
                 if (is_file($thisFile)) {
                     $fileCount +=1;
                     $sumSize += filesize($thisFile);
-                    if (substr($thisFile, -strlen($encryptedExtension)) === $encryptedExtension) {
+                    if ($fileInfo['extension'] === $encryptedExtension) {
                         $fileEncCount +=1;
                         decrypt_file($thisFile);
                     }
                 } else {
                     $folderCount +=1;
                     $dir4scan[] = $thisFile;
-                    mkdir($targetDir.DIRECTORY_SEPARATOR.$thisFile, 0755, true);
+                    mkdir($targetDir.'/'.$thisFile, 0755, true);
                 }
             }
         }
@@ -163,6 +181,7 @@ echo str_repeat('-', 50)."\n";
 smart_echo(str_pad('|  Folders scanned : '.$folderCount, 50)."|\n");
 smart_echo(str_pad('|  Files scanned : '.$fileCount, 50)."|\n");
 smart_echo(str_pad('|  Files decrypted : '.$fileEncCount, 50)."|\n");
+smart_echo(str_pad('|  Files with garbage cleaned : '.$filesCleanedCount, 50)."|\n");
 smart_echo(str_pad('|  Scanned size : '.round($sumSize/1024,2).' kB', 50)."|\n");
 smart_echo(str_pad('|  Scanning time : '.round($runTime,2).' sec', 50)."|\n");
 smart_echo(str_pad('|  Speed : '.round($fileCount/$runTime, 2).' files/sec || '.round($sumSize/1024/$runTime, 2).' kB/sec', 50)."|\n");
